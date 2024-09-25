fd3c53b45d75b218a67116a7bd754711de6993c4
Elsa <eperelli@mitre.org>
2024-09-25 16:11:35 -0400

    Upgrade inferno template with step 2 solution

--- lib/inferno_template.rb
+++ lib/inferno_template.rb
@@ -57,5 +57,24 @@ module InfernoTemplate
     # Tests and TestGroups can be written in separate files and then included
     # using their id
     group from: :patient_group
+
+    group do
+      id :search_tests
+      title 'Search Tests'
+
+      input :patient_id
+
+      test do
+        title 'Condition Search by Patient'
+
+        run do
+          fhir_search('Condition', params: { patient: patient_id })
+
+          assert_response_status(200)
+          assert_resource_type('Bundle')
+          assert_valid_bundle_entries
+        end
+      end
+    end
   end
 end
